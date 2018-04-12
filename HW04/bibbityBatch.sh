2b06a258463d4770c00d21ffe7d50c87cd2b4ffc
Colin Cummins <colinc1@nrlogin7.cluster>
2018-04-12 11:00:43 -0400

    Update

---
+++ HW04/bibbityBatch.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+#
+#PBS -l walltime=00:15:00
+#PBS -l nodes=1:ppn=20
+#PBS -W group_list=newriver
+#PBS -q open_q
+#PBS -j oe
+
+
+cd $PBS_O_WORKDIR
+
+module purge
+module load gcc
+
+make
+./main 1
+./main 2
+./main 4 
+./main 8
+./main 16
+./main 20
